c58c3f18f62a34c375ed39c0ad55149d4cc23087
megabayt <megabayt_m@mail.ru>
2018-09-05 13:36:38 +0300

    added whatsapp and telegram

--- index.js
+++ index.js
@@ -3,21 +3,46 @@ import { Platform, Linking } from 'react-native'
 const isString = (str) => Object.prototype.toString.call(str) === '[object String]'
 const isBool = (bool) => Object.prototype.toString.call(bool) === '[object Boolean]'
 
-const viber = async (args = {}) => {
-
-  if (!args.number) { return Promise.reject('no number provided') }
-  if (!isString(args.number)) { return Promise.reject('number should be string') }
-
-  const url = `viber://add?number=${args.number}`;
-
+const openApp = async (url, iosLink, androidLink) => {
   try {
     return await Linking.openURL(url);
   } catch(err) {
     const urlMarket = Platform.OS === 'ios'
-      ? `itms-apps://itunes.apple.com/us/app/id382617920?mt=8`
-      : `market://details?id=com.viber.voip`;
+      ? iosLink
+      : androidLink;
     return await Linking.openURL(urlMarket);
   }
 }
 
-export default viber;
+export const openViber = async (args = {}) => {
+  if (!args.number) { return Promise.reject('no number provided') }
+  if (!isString(args.number)) { return Promise.reject('number should be string') }
+
+  return await openApp(
+    `viber://add?number=${args.number}`,
+    'itms-apps://itunes.apple.com/us/app/id382617920?mt=8',
+    'market://details?id=com.viber.voip'
+  );
+}
+
+export const openWhatsapp = async (args = {}) => {
+  if (!args.number) { return Promise.reject('no number provided') }
+  if (!isString(args.number)) { return Promise.reject('number should be string') }
+
+  return await openApp(
+    `https://api.whatsapp.com/send?phone=+${args.number}`,
+    'itms-apps://itunes.apple.com/us/app/id310633997?mt=8',
+    'market://details?id=com.whatsapp'
+  );
+}
+
+export const openTelegramm = async (args = {}) => {
+  if (!args.number) { return Promise.reject('no number provided') }
+  if (!isString(args.number)) { return Promise.reject('number should be string') }
+
+  return await openApp(
+    `tg://resolve?domain=${args.number}`,
+    'itms-apps://itunes.apple.com/us/app/id686449807?mt=8',
+    'market://details?id=org.telegram.messenger'
+  );
+}
